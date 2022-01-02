424eda6810fb765e1c97daf3e547022ea37c4704
John3107 <maynsayt@gmail.com>
2022-01-03 00:06:37 +0200

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>JSONPlaceholder is a free online REST API that you can use whenever you need some fake data. It can be in a README on GitHub, for a demo on CodeSandbox, in code examples on Stack Overflow, ...or simply to test things locally.</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
---
+++ src/views/Todos.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h2>Todo application</h2>
+    <router-link to="/">Home</router-link>
+    <hr />
+    <AddTodo @add-todo="addTodo" />
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="active">Active</option>
+    </select>
+    <hr />
+    <Loader v-if="loading" />
+    <TodoList
+      v-else-if="filteredTodos.length"
+      v-bind:todos="filteredTodos"
+      @remove-todo="removeTodo"
+    />
+    <p v-else>No todos!</p>
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList";
+import AddTodo from "@/components/AddTodo";
+import Loader from "@/components/Loader";
+export default {
+  name: "App",
+  data() {
+    return {
+      todos: [],
+      loading: true,
+      filter: "all",
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=3")
+      .then((response) => response.json())
+      .then((json) => {
+        setTimeout(() => {
+          this.todos = json;
+          this.loading = false;
+        }, 1000);
+      });
+  },
+  computed: {
+    filteredTodos() {
+      if (this.filter === "all") {
+        return this.todos;
+      } else if (this.filter === "completed") {
+        return this.todos.filter((t) => t.completed);
+      } else if (this.filter === "active") {
+        return this.todos.filter((t) => !t.completed);
+      } else {
+        return [];
+      }
+    },
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((t) => t.id !== id);
+    },
+    addTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+  components: {
+    TodoList,
+    AddTodo,
+    Loader,
+  },
+};
+</script>
